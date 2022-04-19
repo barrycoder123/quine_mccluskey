6bd52d1da1c4c1297e9d3d4a5913c4379098d904
Ibrahima Barry <ibarry01@tufts.edu>
2022-04-19 19:19:04 -0400

    implemented pi dict

--- qm.py
+++ qm.py
@@ -42,25 +42,39 @@ def group_matching(G1, G2):
                 match_arrays.append([int1, int2, diff])
     return match_arrays
 
+def min_dist(arr):
+    arr = sorted(arr)
+    diff = 10**20 # big number
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if abs(arr[i] - arr[j]) < diff:
+                diff = abs(arr[i] - arr[j])
+    return diff
+
+
 def larger_group_matching(G1, G2):
     # idea: using the arrays from group_matching function
     # check if the last elements match
     match_arrays = []
+    ans = []
     for i in range(len(G1)):
         for j in range(len(G2)):
             if G1[i][2] == G2[j][2]: # the last array element matches so can match them
                 # idea just concatenate them first take the first two of each
-                # and concatenate the min and max differences between the
-                # numbers
-                match_arrays.append([ G1[i][0:2] + G2[j][0:2] ])
+                ans = [ G1[i][0:2] + G2[j][0:2] ]
+                match_arrays.append(ans[0])
     # remove redundancies from list (because im lazy)
-    match_arrays_x = []
     # sort all arrays in match arrays
     for i in match_arrays:
-        i[0] = sorted(i[0]);
-        if i[0] not in match_arrays_x:
-            match_arrays_x.append(i[0])
-    return match_arrays_x
+        for j in match_arrays:
+            if set(i) == set(j):
+                match_arrays.remove(i);
+
+    for i in match_arrays:
+        i.append(min_dist(i))
+        i.append(abs(i[0] - i[1]))
+
+    return match_arrays
 
 def list_to_binary(array):
     bin_array = [ ]
@@ -132,8 +146,12 @@ def cubes(minterms, dont_cares):
         if bin_dc[i].count('1') == 4:
             four_ones.append(dont_cares[i])
             print(dont_cares[i], end=" ");
-    #TODO checking which 0 cubes to cross out
+
     print("\n")
+    #TODO checking which 0 cubes to cross out
+    #idea: put all the 0-cubes in a dictionary and check them off (true/false)
+    zero_cubes_dict = {key: False for key in minterms + dont_cares}
+    print(zero_cubes_dict);
     print("----------------------------------------------------------------")
     print("1-cubes groups 0 and 1")
     print("----------------------------------------------------------------")
@@ -155,10 +173,19 @@ def cubes(minterms, dont_cares):
     print("----------------------------------------------------------------")
     print("1-cubes groups 3 and 4")
     print("----------------------------------------------------------------")
-    print(three_ones, four_ones);
     match_arrays4 = group_matching(list_to_binary(three_ones),
             list_to_binary(four_ones))
     print(match_arrays4)
+
+    print("\n");
+    print("----------------------------------------------------------------")
+    print("these are the zero cubes that have been checked off")
+    print("----------------------------------------------------------------")
+    for i in zero_cubes_dict:
+        if i in match_arrays or match_arrays2 or match_arrays3 or match_arrays4:
+            zero_cubes_dict[i] = True;
+
+    print(zero_cubes_dict)
     #TODO checking which 1 cubes to cross out
     print("\n")
     print("----------------------------------------------------------------")
@@ -171,6 +198,52 @@ def cubes(minterms, dont_cares):
     print("----------------------------------------------------------------")
     match_arrays6 = larger_group_matching(match_arrays2, match_arrays3)
     print(match_arrays6)
+    print("----------------------------------------------------------------")
+    print("2-cubes groups 2* and 3*")
+    print("----------------------------------------------------------------")
+    match_arrays7 = larger_group_matching(match_arrays3, match_arrays4)
+    print(match_arrays7)
+    one_cubes_dict = {}
+    for i in match_arrays, match_arrays2, match_arrays3, match_arrays4:
+        one_cubes_dict = {tuple(i) : False for i in  match_arrays +
+                match_arrays2 + match_arrays3 + match_arrays4}
+
+    print(one_cubes_dict)
+
+    print("----------------------------------------------------------------")
+    print("these are the one cubes that have been checked off")
+    print("----------------------------------------------------------------")
+    for i in one_cubes_dict:
+        if i in match_arrays5 or match_arrays6 or match_arrays7:
+            one_cubes_dict[i] = True;
+    print(one_cubes_dict);
+
+    # prime implicant list
+    pi = []
+    for i in zero_cubes_dict:
+        if zero_cubes_dict[i] == False:
+            pi.append(i)
+    for i in one_cubes_dict:
+        if one_cubes_dict[i] == False:
+            pi.append(i)
+    for i in match_arrays5, match_arrays6, match_arrays7:
+        if i != []:
+            pi.append(i)
+    pi_dict = {}
+    reshape = []
+    for i in pi:
+        for j in range(len(i)):
+            reshape.append(i[j])
+    print(reshape);
+    for i in range(len(reshape)):
+        for j in reshape:
+            pi_dict[i] = j
+    print("----------------------------------------------------------------")
+    print("these are the prime implicants")
+    print("----------------------------------------------------------------")
+    # print(pi)
+    print(pi_dict)
+    print("\n");
 
 if __name__ == "__main__":
     minterms = [1, 5, 7, 8, 9, 13, 15]
